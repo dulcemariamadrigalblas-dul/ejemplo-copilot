e49f14a31c1b463d8f1fd37704d765644ce05edb
Dulce Blas <dulcemariamadrigalblas@gmail.com>
2026-09-24 14:25:31 -0600

    git

---
+++ soluciones/salario_semanal.py
@@ -0,0 +1,58 @@
+''' 
+PLANTEAMINETO DEL PROBLEMA 
+
+Calcula el salario semanal de 
+un trabajador. Las horas 
+mayores a 40 se pagan al 
+doble
+'''
+
+# PROBLEMA: calcular salario semanal
+# ENTRADAS: horas_trabajadas, pago_por_hora
+# SALIDA: salario_semanal
+# ALGORITMO
+# 1. Leer horas trabajadas
+# 2. Leer pago por hora
+# 3. Si horas <= 40:
+#       salario = horas * pago
+# 4. Si no:
+#       extra = horas - 40
+#       salario = (40 * pago) + (extra * pago * 2)
+# 5. Mostrar salario
+
+#CONTRATO DE FUNCIONES 
+#leerDatos()
+#Entrada: ninguna
+#Salida: horas y pago
+#Responsabilidad: pedir datos al usuario 
+
+#CalcularSalario(horas, pago)
+#Entrada: horas, pago
+#Salida: salario
+#Responsabilidad: 
+#calcular, no imprimir
+
+#mostrarSalario(salario)
+#Entrada: salario
+#Salida: ninguna
+#Responsabilidad: 
+#mostrar resultado
+
+#CASOS DE PRUEBA
+#Caso 1: 40 horas a $10 
+#Entrada: 40, 10
+#Salida: 400
+#Caso 2: 45 horas a $10 
+#Entrada: 45, 10    
+#Salida: 475
+#Caso 3: 50 horas a $12
+#Entrada: 50, 12
+#Salida: 600
+
+#Restricciones:
+# - no imprimir dentro de la funcion
+# - devolver el resultado
+# - no usar bibliotecas externas
+# - no usar variables globales
+# - no realices llamadas a funciones dentro de este archivo 
+
